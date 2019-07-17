2b18da23f8e8ed3f35f85dee817ac78251dc50ae
Dmitry Zaytsev <bracadabra@yandex-team.ru>
2019-07-17 10:01:00 +0300

    Move dependencies to buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,10 @@
+object Versions {
+    val kotlin = "1.3.31"
+    val androidx = "1.0.2"
+}
+
+object Dependencies {
+    val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val appcompat = "androidx.appcompat:appcompat:${Versions.androidx}"
+    val coreKtx = "androidx.core:core-ktx:${Versions.androidx}"
+}
